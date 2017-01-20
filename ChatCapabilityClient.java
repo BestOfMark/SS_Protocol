83f97e8c36b6aa63ab17804eae2229530112ed82
BestOfMark <m.krone@student.utwente.nl>
2017-01-20 20:27:47 +0100

    Update ChatCapabilityClient.java

--- ChatCapabilityClient.java
+++ ChatCapabilityClient.java
@@ -3,8 +3,10 @@ package connect4;
 public interface ChatCapabilityClient {
 
 	/**
-	 * Send a chat message to the server, which in his turn will broadcast it to all capable clients
+	 * Send a chat message to the server, which in his turn will broadcast it to (all) capable clients
 	 * @param msg the message to be sent
+	 * @param recipientID the server-generated id to whom the message should be sent to. To send to
+	 * everyone use -1.
 	 */
-	public void cmdChat(String msg);
+	public void cmdChat(int recipientID, String msg);
 }
