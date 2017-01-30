72dbbfc0ebd902a86f7ffed2b0a98c5f005cd891
BestOfMark <m.krone@student.utwente.nl>
2017-01-30 13:05:42 +0100

    Update ChatCapabilityServer.java

--- ChatCapabilityServer.java
+++ ChatCapabilityServer.java
@@ -4,8 +4,9 @@ public interface ChatCapabilityServer {
 	
 	/**
 	 * Sends an user-message to all capable clients, also to the client the MESSAGE command came from.
+	 * @param senderId the id of the client that has sent the CHAT command
 	 * @param msg the message to broadcast
 	 */
-	public void cmdBroadcastMessage(String msg);
+	public void cmdBroadcastMessage(int senderId, String msg);
 
 }
